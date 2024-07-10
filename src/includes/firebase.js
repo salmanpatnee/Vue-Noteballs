f01ed8ec1a51484456d63b87b61a0fa0b66b1836
salmanpatnee <salmanpatni92@gmail.com>
2024-07-10 23:55:13 +0500

    Install firebase.

---
+++ src/includes/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyByrngBqd-vPRabfFobr9QiHtXD6j8FzaA',
+  authDomain: 'vue-noteballs-92596.firebaseapp.com',
+  projectId: 'vue-noteballs-92596',
+  storageBucket: 'vue-noteballs-92596.appspot.com',
+  messagingSenderId: '899984865434',
+  appId: '1:899984865434:web:c81a8d3f2462a4fe6ac723'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
